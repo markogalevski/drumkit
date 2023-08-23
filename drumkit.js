1d5650c51d91fd80972510673463553414d3b198
Marko Galevski <galevski.marko+github@gmail.com>
2023-08-23 13:01:07 +0300

    on click playing

--- drumkit.js
+++ drumkit.js
@@ -1,9 +1,10 @@
-window.addEventListener('keydown', play_sound);
 
-function play_sound(e) {
-	const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-	const key = document.querySelector(`.key[data-key="${e.key}"]`);
-	console.log(key);
+window.addEventListener('keydown', play_sound_key);
+
+
+function play_sound_worker(keycode) {
+	const audio = document.querySelector(`audio[data-key="${keycode}"]`);
+	const key = document.querySelector(`.key[data-key="${keycode}"]`);
 	if (!audio) {
 		return;
 	}
@@ -11,6 +12,10 @@ function play_sound(e) {
 	audio.play();
 	key.classList.add('playing');
 	
+}
+
+function play_sound_key(e) {
+	play_sound_worker(e.key);
 }
 
 function humanise(audio) {
@@ -22,7 +27,12 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => {
 	
 	key.addEventListener('transitionend', remove_transition);
+	key.addEventListener('click', play_sound_click);
 });
+
+function play_sound_click(e) {
+	play_sound_worker(e.target.getAttribute('data-key'));
+}
 
 function remove_transition(e) {
 	if (e.propertyName != 'transform') return;
