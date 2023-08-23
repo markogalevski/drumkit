a0b43c292a65fae77e13bfd86be0c7ddbbcbc168
Marko Galevski <galevski.marko+github@gmail.com>
2023-08-23 12:30:48 +0300

    added js listeners, css

--- drumkit.js
+++ drumkit.js
@@ -1,10 +1,30 @@
-window.addEventListener('keydown', e => {
+window.addEventListener('keydown', play_sound);
+
+function play_sound(e) {
 	const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-	console.log(audio);
+	const key = document.querySelector(`.key[data-key="${e.key}"]`);
+	console.log(key);
 	if (!audio) {
 		return;
 	}
-	audio.load();
+	humanise(audio)
 	audio.play();
+	key.classList.add('playing');
 	
+}
+
+function humanise(audio) {
+	audio.currentTime = Math.random() * 0.04
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => {
+	
+	key.addEventListener('transitionend', remove_transition);
 });
+
+function remove_transition(e) {
+	if (e.propertyName != 'transform') return;
+	this.classList.remove('playing');
+}
